59b3674197fed5898b96753fccb1ccd1f720c6da
Vitor de Lima <vitorcirqueira@inf.ufg.br>
2019-03-27 22:26:57 -0300

    Correção exercicios 8 e 9

--- exercicio8.c
+++ exercicio8.c
@@ -4,7 +4,7 @@
 int main () {
     int n;
     scanf("%d", &n);
-    for (int i=0; i>=10000; i++){
+    for (int i=0; i<=10000; i++){
         if(i%n == 2)
             printf("%d\n", i);
     }
--- exercicio9.c
+++ exercicio9.c
@@ -4,7 +4,7 @@
 int main () {
     int n;
     scanf("%d", &n);
-    for (int i=1; i>=n; i++){
+    for (int i=1; i<=n; i++){
         printf("%d\n", i);
     }
 	system("pause");
